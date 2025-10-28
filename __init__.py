b84fd612e74dc76b6c024d4aeb5dbb6f960d41c6
Amro <amro.eidd@gmail.com>
2025-10-29 01:06:15 +0200

    feat(core): Implement V1 connection testing logic for UI

--- __init__.py
+++ __init__.py
@@ -22,6 +22,13 @@
  ***************************************************************************/
  This script initializes the plugin, making it known to QGIS.
 """
+import os
+import sys
+
+plugin_dir = os.path.dirname(__file__)
+vendor_dir = os.path.join(plugin_dir, 'vendor')
+if vendor_dir not in sys.path:
+    sys.path.insert(0, vendor_dir)
 
 
 # noinspection PyPep8Naming
